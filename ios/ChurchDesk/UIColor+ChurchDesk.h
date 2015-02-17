e3b76e4c0c03f70871127d46f91ca584d3ca5014
Jakob <jakob@jakobs-macpro.local>
2015-02-17 15:00:00 +0100

    added UIColor category

---
+++ ios/ChurchDesk/UIColor+ChurchDesk.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+ChurchDesk.h
+//  ChurchDesk
+//
+//  Created by Jakob Vinther-Larsen on 17/02/15.
+//  Copyright (c) 2015 Shape A/S. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SHPUIKit.h"
+
+@interface UIColor (ChurchDesk)
+
++ (UIColor*) chd_blueColor;
+
+@end
---
+++ ios/ChurchDesk/UIColor+ChurchDesk.m
@@ -0,0 +1,17 @@
+//
+//  UIColor+ChurchDesk.m
+//  ChurchDesk
+//
+//  Created by Jakob Vinther-Larsen on 17/02/15.
+//  Copyright (c) 2015 Shape A/S. All rights reserved.
+//
+
+#import "UIColor+ChurchDesk.h"
+
+@implementation UIColor (ChurchDesk)
+
++ (UIColor*) chd_blueColor {
+  return [UIColor blueColor];
+}
+
+@end
