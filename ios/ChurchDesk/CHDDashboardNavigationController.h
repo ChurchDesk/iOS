97093574ea09640b468929f31002e2245ca63df3
Mikkel Selsøe Sørensen <mikkel@shape.dk>
2015-02-18 09:14:36 +0100

    Fixed build issues

--- ios/ChurchDesk/CHDDashboardNavigationController.h
+++ ios/ChurchDesk/CHDDashboardNavigationController.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Shape A/S. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>;
+#import <UIKit/UIKit.h>
 #import "SHPSideMenuController.h"
 
 @interface CHDDashboardNavigationController : UINavigationController
